cfe8dfa748b44e9b452f826ce2603ecd7f986daa
GracefulLemming <calebtrevinoe@gmail.com>
2017-05-16 20:31:34 -0500

    Update OmniDrive.java

--- app/src/main/java/com/ba/robolib/Wheels/OmniDrive.java
+++ app/src/main/java/com/ba/robolib/Wheels/OmniDrive.java
@@ -1,10 +1,9 @@
 package com.ba.robolib.Wheels;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-/**
- * Created by LCHERRYHOLMES on 5/16/2017.
- */
+
 
 public class OmniDrive {
-
+  public void translate(){
+  }
 }
